8292186c2a54ce45fb1b3cd22815c48daf40a43f
soci0-fa1l <soci0.pas3@gmail.com>
2025-07-03 05:02:55 +0800

    fix build for vercel

--- src/index.tsx
+++ src/index.tsx
@@ -1,12 +1,14 @@
 // src/index.tsx
 import React from 'react';
-import ReactDOM from 'react-dom';
+import ReactDOM from 'react-dom/client';
 import './index.css';  // 이 부분이 제대로 되어 있어야 합니다.
 import App from './App';
 
-ReactDOM.render(
+const root = ReactDOM.createRoot(
+  document.getElementById('root') as HTMLElement
+);
+root.render(
   <React.StrictMode>
     <App />
-  </React.StrictMode>,
-  document.getElementById('root')
+  </React.StrictMode>
 );
