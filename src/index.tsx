86745a6da83d0780d9967225f8441cbf8b193ed5
soci0-fa1l <soci0.fa1l@gmail.com>
2025-07-02 19:19:44 +0900

    Fix serverless function handler

--- src/index.tsx
+++ src/index.tsx
@@ -1,7 +1,8 @@
+// src/index.tsx
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css'; // 스타일을 원하면 여기에 추가
-import App from './App'; // App 컴포넌트를 import
+import './index.css';  // 이 부분이 제대로 되어 있어야 합니다.
+import App from './App';
 
 ReactDOM.render(
   <React.StrictMode>
