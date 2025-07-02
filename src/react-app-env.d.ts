17e9653d55e61ffd91b93c928fa741e35b21d776
soci0-fa1l <soci0.pas3@gmail.com>
2025-07-02 22:53:56 +0800

    Add LFS asset

--- src/react-app-env.d.ts
+++ src/react-app-env.d.ts
@@ -1,1 +1,9 @@
 /// <reference types="react-scripts" />
+
+declare module 'react-dom' {
+  export function render(
+    element: React.ReactElement,
+    container?: Element | DocumentFragment | null,
+    callback?: () => void
+  ): void;
+}
