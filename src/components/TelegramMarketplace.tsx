32fc0f544e3113c1f294b5a2e63b2a1696927a0d
soci0-fa1l <soci0.fa1l@gmail.com>
2025-07-02 19:39:59 +0900

    Fix serverless function handler

---
+++ src/components/TelegramMarketplace.tsx
@@ -0,0 +1,13 @@
+// src/components/TelegramMarketplace.tsx
+import React from 'react';
+
+const TelegramMarketplace: React.FC = () => {
+  return (
+    <div>
+      <h1>Telegram Marketplace</h1>
+      <p>Welcome to the marketplace!</p>
+    </div>
+  );
+};
+
+export default TelegramMarketplace;
